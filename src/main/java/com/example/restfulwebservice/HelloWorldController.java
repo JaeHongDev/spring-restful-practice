150aa21e1b12e5391b8785be31b0baed6835932e
JaehongDev <println@kakao.com>
2022-08-07 20:54:00 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    /*
+       GET
+       /hello-world (endpoint)
+       @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+     */
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldBean(){
+        return new HelloWorldBean("Hello world");
+    }
+}
---
+++ src/main/java/com/example/restfulwebservice/SpringRestfulPracticeApplication.java
@@ -0,0 +1,11 @@
+package com.example.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestfulPracticeApplication.class, args);
+    }
+}
